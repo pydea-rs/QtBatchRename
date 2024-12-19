dacfcdb02722e40d4aa429f4eefb9f4a1a2c5dec
pya-h <p4ya.py@gmail.com>
2024-12-19 17:50:57 +0000

    implementing appe3nd filename funvtion

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <QDir>
 #include <QFile>
 
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -14,5 +15,26 @@ int main(int argc, char *argv[])
 
 QStringList readFilenames(QString path) {
     QDir dir(path);
+    if(!dir.exists())
+        throw new std::invalid_argument("No such directory!");
     return dir.entryList(QDir::Files);
+}
+
+QString appendFilename(const QString& filename, const QString& newName) {
+    unsigned short int dotIndex = 0;
+    unsigned short oldNameLength = filename.length();
+    for(unsigned short int i = filename.length() - 1; i > 0; i++) {
+        if(!dotIndex && filename[i] == '.')
+            dotIndex = i;
+        if(filename[i] == '/' || filename == '\\') {
+
+        }
+    }
+
+}
+void renameByAppend(QStringList filenames) {
+    for (const auto& filename: filenames) {
+        QFile file(filename);
+        file.file
+    }
 }
